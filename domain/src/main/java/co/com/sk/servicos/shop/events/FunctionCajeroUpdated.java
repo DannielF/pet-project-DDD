bf1d61a8f5b2ed7bfe4095de1f662af7d714de98
riveco0120 <richardsk.vellojin@gmail.com>
2022-05-17 09:43:48 -0500

    Agregando actualizar funcion cajero
    Se implementa el evento para actualizar funcion del cajero

--- domain/src/main/java/co/com/sk/servicos/shop/events/FunctionCajeroUpdated.java
+++ domain/src/main/java/co/com/sk/servicos/shop/events/FunctionCajeroUpdated.java
@@ -1,4 +1,28 @@
 package co.com.sk.servicos.shop.events;
+
+import co.com.sk.servicos.shop.values.CashierEmployeeId;
+import co.com.sk.servicos.shop.values.Function;
+import co.com.sofka.domain.generic.DomainEvent;
+
+import java.util.UUID;
+
 //Cajero Empleado Actualiado
-public class FunctionCajeroUpdated {
+public class FunctionCajeroUpdated extends DomainEvent {
+
+    private final CashierEmployeeId entityId;
+    private final Function function;
+
+    public FunctionCajeroUpdated(CashierEmployeeId entityId, Function function) {
+        super("teveo.shop.functioncajeroupdate");
+        this.entityId=entityId;
+        this.function=function;
+    }
+
+    public CashierEmployeeId getEntityId() {
+        return entityId;
+    }
+
+    public Function getFunction() {
+        return function;
+    }
 }
